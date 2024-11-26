ade2f4412f07e40d534d4a01d1f59496fd827c31
Nestaldo <168582607+Nestaldo@users.noreply.github.com>
2024-11-26 20:47:38 -0300

    guardado por seguridad
    guardado de seguridad antes de hacer el login un objeto estatico entre escenas

--- Eva_3_Progra_4_Benjamin_Burgos/Assets/Scripts/Canvas/CanvasDecimasMenu.cs
+++ Eva_3_Progra_4_Benjamin_Burgos/Assets/Scripts/Canvas/CanvasDecimasMenu.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEditor.PackageManager;
 
 public class CanvasDecimasMenu : MonoBehaviour
 {
@@ -10,9 +14,18 @@ public class CanvasDecimasMenu : MonoBehaviour
     [SerializeField] GameObject ramo2;
     [SerializeField] GameObject ramo3;
 
+    [SerializeField] TMP_Text decimasText;
+
     public void DesactivateVerticalGridLayout()
     {
         VerticalGridLayout.SetActive(false);
+    }
+    public void ReactivateVerticalGridLayout()
+    {
+        VerticalGridLayout.SetActive(true);
+        ramo1.SetActive(false);
+        ramo2.SetActive(false);
+        ramo3.SetActive(false);
     }
     public void ActivateRamo1()
     {
@@ -34,5 +47,38 @@ public class CanvasDecimasMenu : MonoBehaviour
         DesactivateVerticalGridLayout();
         ramo1 .SetActive(false);
         ramo2 .SetActive(false);
+    }
+    public void FetchDecimasFromPlayfab()
+    {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), OnDataReceived, OnError);
+    }
+
+    void OnDataReceived(GetUserDataResult result)
+    {
+        if(result.Data != null && result.Data.ContainsKey("playerData"))
+        {
+            string playerDataJson = result.Data["playerData"].Value;
+            PlayerData playerData = JsonUtility.FromJson<PlayerData>(playerDataJson);
+            if (playerData != null)
+            {
+                UpdateDecimasText(playerData.decimas);
+            }
+            else
+            {
+                Debug.LogWarning("No se encontraron las decimas");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Key ´playerData´ not found in PlayFab user data");
+        }
+    }
+    void OnError(PlayFabError msg)
+    {
+        Debug.Log($"Error retrieving data from PlayFab: {msg.GenerateErrorReport()}");
+    }
+    private void UpdateDecimasText(float decimas)
+    {
+        decimasText.text = decimas.ToString();
     }
 }
