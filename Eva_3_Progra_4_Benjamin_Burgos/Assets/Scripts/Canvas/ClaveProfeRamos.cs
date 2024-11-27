cab4ceb2d275ccf7d5c78f41eaa91386b2985e4f
Nestaldo <168582607+Nestaldo@users.noreply.github.com>
2024-11-27 20:41:18 -0300

    escenas distintas cada ramo

--- Eva_3_Progra_4_Benjamin_Burgos/Assets/Scripts/Canvas/ClaveProfeRamos.cs
+++ Eva_3_Progra_4_Benjamin_Burgos/Assets/Scripts/Canvas/ClaveProfeRamos.cs
@@ -5,13 +5,40 @@ using UnityEngine;
 
 public class ClaveProfeRamos : MonoBehaviour
 {
+    [Header("Tmp Cavas")]
     [SerializeField] TMP_Text feedbackText;
     [SerializeField] TMP_InputField inputfieldClave;
+    [Header("Claves Ramos")]
     [SerializeField] string claveProfe;
+    [SerializeField] string claveProfe2;
+    [SerializeField] string claveProfe3;
+    [Header("SceneLoader")]
     [SerializeField] SceneLoaderManager sceneLoaderManager;
-    public void VerifyPassword()
+    public void VerifyPasswordRamo1()
     {
         if(inputfieldClave.text == claveProfe)
+        {
+            sceneLoaderManager.IsLogedStartGame();
+        }
+        else
+        {
+            feedbackText.text = "Wrong Password";
+        }
+    }
+    public void VerifyPasswordRamo2()
+    {
+        if (inputfieldClave.text == claveProfe2)
+        {
+            sceneLoaderManager.IsLogedStartGame();
+        }
+        else
+        {
+            feedbackText.text = "Wrong Password";
+        }
+    }
+    public void VerifyPasswordRamo3()
+    {
+        if (inputfieldClave.text == claveProfe3)
         {
             sceneLoaderManager.IsLogedStartGame();
         }
